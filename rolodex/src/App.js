11fd6c32fd9e8a0418ebbcda13ef495febe4cf6c
Geff <***>
2021-07-13 20:54:48 +0200

    p1
    Init project du p1 et decouverte

---
+++ rolodex/src/App.js
@@ -0,0 +1,28 @@
+import {Component} from "react";
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            name: 'Geff'
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>Hello {this.state.name}</p>
+                    <button onClick={() => this.setState({name: 'Trou de balle'})}>Click Me</button>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default App;
